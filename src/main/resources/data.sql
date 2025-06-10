cf7917a5cee336be565df0d57bb52b414073d557
CodingMasterLSW <spqjekdl1004@gmail.com>
2025-06-10 14:05:12 +0900

    feat: prod 더미 데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO CREW(name, email) VALUES('젠슨','a@com');
+INSERT INTO CREW(name, email) VALUES('포포','b@com');
+INSERT INTO CREW(name, email) VALUES('가이온','c@com');
+INSERT INTO CREW(name, email) VALUES('짱구','d@com');
+INSERT INTO CREW(name, email) VALUES('돔푸','e@com');
+
+INSERT INTO COACH(name, email) VALUES('솔라','a1@com');
+INSERT INTO COACH(name, email) VALUES('리사','a2@com');
+INSERT INTO COACH(name, email) VALUES('네오','a3@com');
